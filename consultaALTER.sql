251ee60390fbdcf027b8b2fa99eb2c20dc4fdd02
EleqfulCEAC <elean.quintero@alu.ceacfp.es>
2025-02-26 13:47:23 +0100

    Clase 26/02/25

--- consultaALTER.sql
+++ consultaALTER.sql
@@ -4,7 +4,7 @@ ALTER TABLE contactos ADD COLUMN ciudad VARCHAR(30) NOT NULL AFTER apellidos;
 /*Al no permitir valores NULOS, sus valors por defectos seran VACIOS*/
 
 
-/*Funcion de agrupacion que cuenta los datos*/ 
+/*Funcion de agrupacion que cuenta los datos y devuelve una tupla*/ 
 SELECT COUNT(ciudad) from contactos;
 
 
@@ -36,6 +36,31 @@ UPDATE contactos
 SET sueldo=24000
 WHERE MOD(cod_contacto, 6)=0;
 
+
+
+
+
+
 /*OPERADORES DE COMPARACION*/
 < Mayor que
 > Menor que
+
+/*DISTINCT Selecciona los valores no repetidos en una columna que se le indique*/
+
+SELECT DISTINCT tipo; 
+
+/*Funciones que devuelven maximos, minimos y la media*/
+MAX()
+MIN()
+AVG()
+
+/*Funcion de agrupacion que suma todos los datos y devuelve una tupla*/
+SUM()
+SELECT SUM(SUELDO) FROM contactos;
+   /*Suma los datos y devuelve la suma total de cada departamento*/ 
+    SELECT SUM(SUELDO) FROM contactos GROUP by departamento;
+
+/*Actualizar filas impares*/
+UPDATE contactos 
+SET departamento = 'Marketing'
+WHERE MOD (cod_contacto, 2)=1;
